2ef6a71b0bb566d0f99d8345fe6ce141aa898127
TresnaAgustina <tresnaa001@gmail.com>
2022-03-04 12:58:12 +0800

    make product banner and brand logo box

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,5 @@ toggle.onclick = function () {
       if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
   }
   window.addEventListener('scroll', scrollHeader)
+
+
